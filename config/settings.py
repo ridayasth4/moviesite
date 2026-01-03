961ea2d165fd4190634f5777427793817bd131af
ridayasth4 <ridaya350@gmail.com>
2026-01-03 23:51:10 +0545

    home page layout fixed

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'movies' / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'movies' / 'static']
 
 # Default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
